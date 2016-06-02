bb30726f1081586012c11e064955d29422bdf747
何凡 <echo000@bogon>
2016-06-02 20:26:10 +0800

    Initial Commit

---
+++ H5Project/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  H5Project
+//
+//  Created by echo000 on 16/6/2.
+//  Copyright © 2016年 echoZZPZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ H5Project/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  H5Project
+//
+//  Created by echo000 on 16/6/2.
+//  Copyright © 2016年 echoZZPZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
